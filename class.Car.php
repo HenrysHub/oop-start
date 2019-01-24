53fd86332da42db4c838bb22ed7ebc0232ab489c
Henry Lehtsi <henry.lehtsi@ametikool.ee>
2019-01-24 15:14:56 +0200

    Initial commit

---
+++ class.Car.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henry.lehtsi
+ * Date: 24.01.2019
+ * Time: 13:26
+ */
+
+class Car
+{
+    public $doors = 4;
+}
+
+
+print_r(new Car());
+
+function add($number1, $number2) {
+    return "test";
+}
